7c21c4eb1ff9d3020daea086a360ff2d7d221dc7
sakthivel9963 <sakthivel9963@gmail.com>
2020-02-17 21:55:32 +0530

    basic form submission

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+interface userProps {}
 
-class Form extends Component {
+interface userState {
+  [x: string]: any;
+}
+class Form extends Component<userProps, userState> {
+  constructor(props: Readonly<userProps>) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
   formSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
+    console.log(
+      `email : ${this.state.email} -> password : ${this.state.password}`
+    );
   };
+
+  valueChange = (e: { target: { id: string; value: any } }) => {
+    const { id, value } = e.target;
+    this.setState({
+      [id]: value,
+    });
+  };
+
   render() {
     return (
       <React.Fragment>
@@ -11,11 +34,21 @@ class Form extends Component {
           <form onSubmit={(e) => this.formSubmit(e)}>
             <div className="form-group">
               <label htmlFor="email">Email address:</label>
-              <input type="email" className="form-control" id="email" />
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                onChange={(e) => this.valueChange(e)}
+              />
             </div>
             <div className="form-group">
               <label htmlFor="pwd">Password:</label>
-              <input type="password" className="form-control" id="pwd" />
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                onChange={(e) => this.valueChange(e)}
+              />
             </div>
             <button type="submit" className="btn btn-success">
               Submit
